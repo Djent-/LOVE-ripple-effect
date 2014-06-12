ecdc6bdaf4d9dc4d893ac64093506fc836249c1f
Djent- <inflames2361@aim.com>
2014-06-12 16:09:42 -0400

    added ripple:echo() to create subripples. added ripple:draw().removed recursive ripple. updates self.index in ripple:update(). added infertility to ripples after they reproduce

--- main.lua
+++ main.lua
@@ -9,44 +9,58 @@ end
 function love.update(dt)
 	if #ripples == 0 then return end
 	for x = 1, #ripples do
-
+		ripple.update(ripples[x], dt, x)
 	end
 end
 
 function love.draw()
 	if #ripples == 0 then return end
 	for x = 1, #ripples do
-
-	end
-end
-
-function recursiveDrawSubripples(parent)
-	if parent.subripple then
-
+		ripple.draw(ripples[x])
 	end
 end
 
 function love.mousereleased(x,y)
-	table.insert(ripples, ripple:new(x, y, 60, 10))
+	table.insert(ripples, ripple:new(x, y, 150, 20))
 end
 
-function ripple:new(x, y, maxRadius, freq)
+function ripple:new(x, y, maxRadius, freq, infertile)
 	local self = {}
 	self.x = x
 	self.y = y
 	self.maxRadius = maxRadius
 	self.radius = 1
 	self.freq = freq
-	self.subripple = nil
+	self.infertile = infertile
 	self.index = #ripples + 1
 	return self
 end
 
-function ripple:update(dt)
-	self.radius = self.radius + dt
-	if self.radius > self.maxRadius then self:kill() end
+function ripple:update(dt, x)
+	if not self then return end
+	self.index = x
+	if self.freq < 1 then self.freq = 1 end
+	--print(#ripples, self.index, dt)
+	self.radius = self.radius + dt * self.freq * 2
+	if self.radius > self.maxRadius then ripple.kill(self) end
+	if self.maxRadius < 5 then ripple.kill(self) end
+	if not self.infertile then
+		if self.radius % self.freq <= dt * 50 then
+			ripple.echo(self)
+			self.infertile = true
+		end
+	end
 end
 
 function ripple:kill()
 	table.remove(ripples, self.index)
+end
+
+function ripple:echo()
+	table.insert(ripples, ripple:new(self.x, self.y, self.maxRadius - self.freq,
+		self.freq * 0.9))
+end
+
+function ripple:draw()
+	love.graphics.circle("line", self.x, self.y, self.radius)
 end
