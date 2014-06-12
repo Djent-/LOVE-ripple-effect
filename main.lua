1cf86bcc180c0fd1c01d3b67bf35e7829db089e1
Djent- <inflames2361@aim.com>
2014-06-12 13:31:54 -0400

    added self.index to ripple:new(). ripple:update() kills ripples that are too big. added ripple:kill(). started a recursive draw method that will be deleted soon

--- main.lua
+++ main.lua
@@ -20,6 +20,12 @@ function love.draw()
 	end
 end
 
+function recursiveDrawSubripples(parent)
+	if parent.subripple then
+
+	end
+end
+
 function love.mousereleased(x,y)
 	table.insert(ripples, ripple:new(x, y, 60, 10))
 end
@@ -32,9 +38,15 @@ function ripple:new(x, y, maxRadius, freq)
 	self.radius = 1
 	self.freq = freq
 	self.subripple = nil
+	self.index = #ripples + 1
 	return self
 end
 
 function ripple:update(dt)
 	self.radius = self.radius + dt
+	if self.radius > self.maxRadius then self:kill() end
+end
+
+function ripple:kill()
+	table.remove(ripples, self.index)
 end
